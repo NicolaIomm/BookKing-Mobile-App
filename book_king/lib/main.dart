7b35671161302a09a62b12309ef1fb745a48fa71
NicolaIomm <alocinalocin@gmail.com>
2020-10-07 19:12:53 +0200

    Fixed TabBar
    Added TabBarController + Listener to unfocus TextField on swapping tab

--- book_king/lib/main.dart
+++ book_king/lib/main.dart
@@ -34,7 +34,7 @@ class BookKing extends StatefulWidget {
   _BookKingState createState() => _BookKingState();
 }
 
-class _BookKingState extends State<BookKing> {
+class _BookKingState extends State<BookKing> with TickerProviderStateMixin {
   bool _fireInitialized = false;
   bool _fireError = false;
 
@@ -54,10 +54,24 @@ class _BookKingState extends State<BookKing> {
     }
   }
 
+  User _loggedUser;
+  bool _isLogged;
+
+  FocusScopeNode currentFocus;
+  TextEditingController _searchFieldController ;
+  TabController _tabController ;
+
+  String _tempMsg = "";
+
   @override
   void initState() {
     initializeFlutterFire();
     super.initState();
+
+    _searchFieldController = TextEditingController();
+    _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
+    _tabController.addListener( () => currentFocus.unfocus() );
+
   }
 
   @override
@@ -66,236 +80,236 @@ class _BookKingState extends State<BookKing> {
     super.dispose();
   }
 
-  User _loggedUser;
-  bool _isLogged = false;
-
-  TextEditingController _searchFieldController = TextEditingController();
-
-  String _tempMsg = "";
-
   @override
   Widget build(BuildContext context) {
 
-    print("[DEBUG] BUILD - Method called.");
+  print("[DEBUG] BUILD - Method called.");
 
-    if(_fireError) {
-      print("[DEBUG] FIRECORE - Error while initializating FireCore.");
-    }
-    if (!_fireInitialized) {
-        print("[DEBUG] FIRECORE - Waiting for first initialization.. ");
-    }
+  if(_fireError) {
+    print("[DEBUG] FIRECORE - Error while initializating FireCore.");
+  }
+  if (!_fireInitialized) {
+    print("[DEBUG] FIRECORE - Waiting for first initialization.. ");
+  }
 
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Center(
-              child: Text(
-                '         BookKing',
-                style: TextStyle(fontSize: 24),
-                )
-            ),
-            actions: [
-              IconButton(
-                  iconSize: 48,
-                  icon: (_loggedUser != null ? Image.network(_loggedUser.photoURL) : Icon(Icons.account_circle)),
-                  onPressed: (_loggedUser != null ? _showUserPageLogged : _showUserPageNotLogged)
-              )],
-          bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.explore), text: 'Explore'),
-                Tab(icon: Icon(Icons.account_balance_wallet), text: 'MyBooks'),
-                Tab(icon: Icon(Icons.addchart), text: 'Transactions')
-              ]
-          ),
+  currentFocus = FocusScope.of(context);
+
+  return DefaultTabController(
+    length: 3,
+    child: Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+              '         BookKing',
+              style: TextStyle(fontSize: 24),
+            )
         ),
-        body: TabBarView(
+        actions: [
+          IconButton(
+              iconSize: 48,
+              icon: (_loggedUser != null ? Image.network(_loggedUser.photoURL) : Icon(Icons.account_circle)),
+              onPressed: (_loggedUser != null ? _showUserPageLogged : _showUserPageNotLogged)
+          )],
+        bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(icon: Icon(Icons.explore), text: 'Explore'),
+              Tab(icon: Icon(Icons.account_balance_wallet), text: 'MyBooks'),
+              Tab(icon: Icon(Icons.addchart), text: 'Transactions')
+            ]
+        ),
+      ),
+      body: TabBarView(
+          controller: _tabController,
           children: [
-                        // Explore Tab
+  // Explore Tab
             Padding(
               padding: EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 0.0),
               child: Column(
-                children: [
-                  SizedBox(
-                    height: 40, // set this
-                    child: TextField(
-                        maxLines: 1,
-                        controller: _searchFieldController,
-                        style: TextStyle(fontSize: 18),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(bottom: 18.0),
-                          hintText: 'Search for books ...',
-                          prefixIcon: Icon(Icons.search),
-                          suffixIcon: IconButton(
-                            icon: Icon(Icons.clear),
-                            onPressed: (){
-                              _searchFieldController.clear();
-                              setState((){
-                                _tempMsg = "";
-                              });
-                            },
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20.0),
+                  children: [
+                    SizedBox(
+                        height: 40, // set this
+                        child: TextField(
+                          maxLines: 1,
+                          controller: _searchFieldController,
+                          style: TextStyle(fontSize: 18),
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(bottom: 18.0),
+                            hintText: 'Search for books ...',
+                            prefixIcon: Icon(Icons.search),
+                            suffixIcon: IconButton(
+                              icon: Icon(Icons.clear),
+                              onPressed: (){
+                                _searchFieldController.clear();
+                                setState((){
+                                  _tempMsg = "";
+                                });
+                                },
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20.0),
+                              ),
                             ),
                           ),
-                        ),
-                      onChanged: (value) {
+                          onChanged: (value) {
                             setState((){
                               _tempMsg = value;
                             });
-                          },
-                    )
-                  ),
-                  Text(_tempMsg)
-                ]
+                            },
+                        )
+                    ),
+                    Text(_tempMsg)
+                  ]
               ),
             ),
-                        // MyBooks Tab
+  // MyBooks Tab
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Nothing to show.")
+                Text("Nothing to show."),
               ],
             ),
-                        // Transaction Tab
+  // Transaction Tab
             Center(
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Nothing to show.")
-                  ],
-                ),
-              )
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Nothing to show.")
+                ],
+              ),
+            )
           ]
-        ),
-        /*
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {print(_searchFieldController.text)},
-          child: Icon(Icons.search),
-        )
-        */
+      ),
+      /*
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print(_searchFieldController.text)},
+        child: Icon(Icons.search),
       )
-    );
+      */
+      )
+  );
   }
 
-  void _showUserPageLogged() {
-    Navigator.of(context).push(
-        MaterialPageRoute(builder: (BuildContext context) {
 
-          return Scaffold(
-              appBar: AppBar(title: Text('User Page')),
-              body: Center(
-                  child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                          Column(
-                            children: [
-                              Image.network(_loggedUser.photoURL),
-                              Padding( padding: const EdgeInsets.only(top: 10.0) ),
-                              Text(_loggedUser.displayName),
-                              Padding( padding: const EdgeInsets.only(top: 5.0) ),
-                              Text(_loggedUser.email),
-                              Padding( padding: const EdgeInsets.only(top: 5.0) ),
-                              Text(_loggedUser.phoneNumber != null ? _loggedUser.phoneNumber : "")
-                            ]
-                        ),
-                        const SizedBox(height: 12),
-                        OutlineButton(
-                            splashColor: Colors.grey,
-                            onPressed: () {
-                              signOutGoogle().then((result) {
-                                setState((){
-                                  _isLogged = false;
-                                  _loggedUser = null;
-                                });
-                                Navigator.pop(context);
-                              });
-                            },
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                            highlightElevation: 0,
-                            borderSide: BorderSide(color: Colors.grey),
-                            child: Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                                child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                          'Logout',
-                                          style: TextStyle(
-                                            fontSize: 20,
-                                            color: Colors.grey,
-                                          )
-                                      )]
-                                )
-                            )
-                        )]
-                  )
-              )
-          );
-        })
-    );
+  void _showUserPageLogged() {
+  Navigator.of(context).push(
+  MaterialPageRoute(builder: (BuildContext context) {
+
+  return Scaffold(
+  appBar: AppBar(title: Text('User Page')),
+  body: Center(
+  child: Column(
+  mainAxisSize: MainAxisSize.min,
+  children: [
+  Column(
+  children: [
+  Image.network(_loggedUser.photoURL),
+  Padding( padding: const EdgeInsets.only(top: 10.0) ),
+  Text(_loggedUser.displayName),
+  Padding( padding: const EdgeInsets.only(top: 5.0) ),
+  Text(_loggedUser.email),
+  Padding( padding: const EdgeInsets.only(top: 5.0) ),
+  Text(_loggedUser.phoneNumber != null ? _loggedUser.phoneNumber : "")
+  ]
+  ),
+  const SizedBox(height: 12),
+  OutlineButton(
+  splashColor: Colors.grey,
+  onPressed: () {
+  signOutGoogle().then((result) {
+  setState((){
+  _isLogged = false;
+  _loggedUser = null;
+  });
+  Navigator.pop(context);
+  });
+  },
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+  highlightElevation: 0,
+  borderSide: BorderSide(color: Colors.grey),
+  child: Padding(
+  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+  child: Row(
+  mainAxisSize: MainAxisSize.min,
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+  Text(
+  'Logout',
+  style: TextStyle(
+  fontSize: 20,
+  color: Colors.grey,
+  )
+  )]
+  )
+  )
+  )]
+  )
+  )
+  );
+  })
+  );
   }
 
   void _showUserPageNotLogged() {
-    Navigator.of(context).push(
-        MaterialPageRoute(builder: (BuildContext context) {
-          return Scaffold(
-              appBar: AppBar(title: Text('User Page')),
-              body: Center(
-                  child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 100, 0, 10),
-                        ),
-                        OutlineButton(
-                            splashColor: Colors.grey,
-                            onPressed: () {
-                              signInGoogle().then((result){
-                                if (result != null){
-                                  setState((){
-                                    _isLogged = true;
-                                    _loggedUser = result;
-                                  });
-                                  Navigator.pop(context);
-                                }
-                              });
-                            },
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                            highlightElevation: 0,
-                            borderSide: BorderSide(color: Colors.grey),
-                            child: Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                                child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
-                                      Padding(
-                                          padding: const EdgeInsets.only(left: 10),
-                                          child: Text(
-                                              'Sign in with Google',
-                                              style: TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.grey,
-                                              )
-                                          )
-                                      )
-                                    ])
-                            )
-                        )]
-                  )
-              )
-          );
-        })
-    );
+  Navigator.of(context).push(
+  MaterialPageRoute(builder: (BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(title: Text('User Page')),
+  body: Center(
+  child: Column(
+  mainAxisSize: MainAxisSize.min,
+  children: [
+  Padding(
+  padding: const EdgeInsets.fromLTRB(0, 100, 0, 10),
+  ),
+  OutlineButton(
+  splashColor: Colors.grey,
+  onPressed: () {
+  signInGoogle().then((result){
+  if (result != null){
+  setState((){
+  _isLogged = true;
+  _loggedUser = result;
+  });
+  Navigator.pop(context);
+  }
+  });
+  },
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+  highlightElevation: 0,
+  borderSide: BorderSide(color: Colors.grey),
+  child: Padding(
+  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+  child: Row(
+  mainAxisSize: MainAxisSize.min,
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+  Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+  Padding(
+  padding: const EdgeInsets.only(left: 10),
+  child: Text(
+  'Sign in with Google',
+  style: TextStyle(
+  fontSize: 20,
+  color: Colors.grey,
+  )
+  )
+  )
+  ])
+  )
+  )]
+  )
+  )
+  );
+  })
+  );
   }
 
   void _findBook(){
 
   }
+
 }
+
 
